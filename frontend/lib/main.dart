2f766545a529c0fe18e44f67af2ab26a3e2b02df
alixxxx-x <alix20041124@gmail.com>
2026-01-12 23:12:02 +0100

    new

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app.dart';
+import 'features/authentication/widgets/auth_gate.dart';
 
 void main() {
   runApp(const ProviderScope(child: CampusConnectApp()));
+}
+
+class CampusConnectApp extends StatelessWidget {
+  const CampusConnectApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Campus Connect',
+      debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
+    );
+  }
 }
